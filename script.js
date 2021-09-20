c6473bfbc63c483ecaa8b1e36a7d2d99a869d742
Yuliya Kazakova <julia.kazakova23@gmail.com>
2021-09-21 02:46:00 +0400

    feat: add burger-menu

--- script.js
+++ script.js
@@ -1,1 +1,19 @@
 'use strict'
+
+const hamburger = document.querySelector('.menu__icon');
+const menuList = document.querySelector('.menu__list');
+const menuLink = document.querySelectorAll('.menu__link');
+
+hamburger.addEventListener('click', openMenu);
+
+function openMenu() {
+    hamburger.classList.toggle('active');
+    menuList.classList.toggle('active');
+}
+
+menuLink.forEach(n => n.addEventListener('click', closeMenu));
+
+function closeMenu() {
+    hamburger.classList.remove('active');
+    menuList.classList.remove('active');
+}
